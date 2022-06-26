d766ea3c501f8c56a66b5299642d222d6e7d0991
mohamadElEaswy <midoo_303@yahoo.com>
2022-06-26 15:02:47 +0200

    downloading image error fixed i used the wrong url

--- lib/src/features/image_preview/ui/image_preview_page.dart
+++ lib/src/features/image_preview/ui/image_preview_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_downloader/image_downloader.dart';
 import '../../../commons/gloabl_network_iage_widget.dart';
+import '../../../network/network.dart';
 
 class ImagePreviewPage extends StatelessWidget {
   const ImagePreviewPage({Key? key, required this.imageUrl}) : super(key: key);
@@ -11,7 +13,40 @@ class ImagePreviewPage extends StatelessWidget {
           actions: [
             ElevatedButton.icon(
               onPressed: () async {
-                await ImageDownloader.downloadImage(imageUrl);
+                // await ImageDownloader.downloadImage(imageUrl);
+                // try {
+                //   // Saved with this method.
+                //   var imageId = await ImageDownloader.downloadImage(
+                //     imageUrl,
+                //     destination: AndroidDestinationTypeinExternalFilesDir(),
+                //   );
+                //   if (imageId == null) {
+                //     return;
+                //   }
+
+                //   // Below is a method of obtaining saved image information.
+                //   var fileName = await ImageDownloader.findName(imageId);
+                //   var path = await ImageDownloader.findPath(imageId);
+                //   var size = await ImageDownloader.findByteSize(imageId);
+                //   var mimeType = await ImageDownloader.findMimeType(imageId);
+                //   imageId;
+                // } on PlatformException catch (error) {
+                //   print(error);
+                // }
+
+                ImageDownloader.downloadImage(
+                  NetworkHelper.imageUrl(imageUrl),
+                ).catchError((error) {
+                  if (error is PlatformException) {
+                    var path = "";
+                    if (error.code == "404") {
+                      print("Not Found Error.");
+                    } else if (error.code == "unsupported_file") {
+                      print("UnSupported FIle Error.");
+                      path = error.details["unsupported_file_path"];
+                    }
+                  }
+                });
               },
               icon: const Icon(Icons.download),
               label: const Text('Download'),
